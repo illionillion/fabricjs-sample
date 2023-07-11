508c684e1cc85749928f8f67cc70b1354752f07b
yuto ichinose <yuto.0526.sano@gmail.com>
2023-07-11 12:10:24 +0900

    クリア機能・カラーピッカー

--- js/main.js
+++ js/main.js
@@ -1,45 +1,72 @@
 "use strict";
 
-const state = {
-    mode: 'draw'
-}
+(() => {
+  const state = {
+    canvas: undefined,
+    screen: undefined,
+    color: "",
+    mode: "draw",
+  };
 
-const main = () => {
+  const main = () => {
+    const canvas = new fabric.Canvas("canvas");
+    const screen = document.querySelector(".screen");
+    const colorPicker = document.getElementById("color");
+    state.color = colorPicker.value;
+    state.canvas = canvas;
+    state.screen = screen;
 
-  const canvas = new fabric.Canvas("canvas");
-  const screen = document.querySelector(".screen");
+    resize();
+    window.addEventListener("resize", resize);
+    document.getElementById("draw").addEventListener("click", draw);
+    document.getElementById("erase").addEventListener("click", erase);
+    document.getElementById("clear").addEventListener("click", clear);
+    colorPicker.addEventListener("input", colorChange);
 
-  resize(canvas, screen);
-  window.addEventListener("resize", () => resize(canvas, screen));
-  document.getElementById('draw').addEventListener('click', () => draw(canvas))
-  document.getElementById('erase').addEventListener('click', () => erase(canvas))
+    draw(canvas);
+  };
 
-  draw(canvas)
-};
+  const draw = () => {
+    const canvas = state.canvas;
+    state.mode = "draw";
+    canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
+    canvas.freeDrawingBrush.width = 5;
+    canvas.freeDrawingBrush.color = state.color;
+    canvas.isDrawingMode = true;
+  };
 
-const draw = (canvas) => {
-  canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
-  canvas.freeDrawingBrush.width = 5;
-  canvas.freeDrawingBrush.color = "black";
-  canvas.isDrawingMode = true;
-};
+  const erase = () => {
+    const canvas = state.canvas;
+    state.mode = "erase";
+    canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
+    canvas.freeDrawingBrush.width = 10;
+    canvas.freeDrawingBrush.color = "white";
+    canvas.isDrawingMode = true;
+  };
 
-const erase = (canvas) => {
-  canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
-  canvas.freeDrawingBrush.width = 10;
-  canvas.freeDrawingBrush.color = "white";
-  canvas.isDrawingMode = true;
-};
+  const resize = () => {
+    const canvas = state.canvas;
+    const screen = state.screen;
+    // 画面の横幅を取得
+    const screenWidth = document.documentElement.clientWidth;
+    // 縦幅いっぱいを取得
+    const screenHeight = screen.clientHeight;
 
-const resize = (canvas, screen) => {
-  // 画面の横幅を取得
-  const screenWidth = window.innerWidth;
-  // 縦幅いっぱいを取得
-  const screenHeight = screen.clientHeight;
+    canvas.setWidth(screenWidth);
+    canvas.setHeight(screenHeight);
+    canvas.renderAll();
+  };
 
-  canvas.setWidth(screenWidth);
-  canvas.setHeight(screenHeight);
-  canvas.renderAll();
-};
+  const clear = () => {
+    const canvas = state.canvas;
+    canvas.clear();
+  };
 
-window.addEventListener("load", main);
+  const colorChange = (e) => {
+    state.color = e.target.value;
+    if (state.mode === "draw")
+      state.canvas.freeDrawingBrush.color = state.color;
+  };
+
+  window.addEventListener("load", main);
+})();
